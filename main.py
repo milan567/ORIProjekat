185d2196517e6ce649cda2c8dd93861c19674e77
Nikolina <rnikolina1202@gmail.com>
2017-07-05 18:34:32 +0200

    regresija i neuronska kao posebni modeli

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+from sklearn.linear_model import LinearRegression
 
 
-def rmse(prediction, ground_truth):
-    prediction = prediction[ground_truth.nonzero()].flatten()
-    ground_truth = ground_truth[ground_truth.nonzero()].flatten()
-    return sqrt(mean_squared_error(prediction, ground_truth))
+def eval_cf(prediction, truth):
+    prediction = prediction[truth.nonzero()].flatten()
+    truth = truth[truth.nonzero()].flatten()
+    return sqrt(mean_squared_error(prediction,truth))
+
+
+def eval_mlp(prediction, truth):
+    to_eval_real = []
+    to_eval_predicted = []
+    for p in prediction:
+        to_eval_predicted.append(float(p))
+    for p in truth:
+        to_eval_real.append(float(p))
+    return sqrt(mean_squared_error(to_eval_predicted, to_eval_real))
 
 def predict(ratings, similarity):
     rated_num = numpy.count_nonzero(ratings, axis=1)
@@ -53,17 +64,20 @@ if __name__=='__main__':
     test_data_matrix = numpy.zeros((n_users, movie_num))
     for line in test_data.itertuples():
         test_data_matrix[line[1]-1, line[2]-1] = line[3]
-
     user_similarity = pairwise_distances(train_data_matrix, metric='cosine')
+    #predikcija za sve korisnike preko matrica
     predicted = predict(train_data_matrix, user_similarity)
-    print 'User-based CF RMSE: ' + str(rmse(predicted, test_data_matrix))
+
+    print 'User-based CF RMSE: ' + str(eval_cf(predicted, test_data_matrix))
+
+    #uzmemo jednog korisnika
     new_sim = predicted[4]
     rated_indexes = train_data_matrix[4].nonzero()[0]
     rated_movies = []
     ratings_movies = []
-    """
-    train_data_ind, test_data_ind = model_selection.train_test_split(rated_indexes, test_size=0.20)
-    for el in train_data_ind:
+
+    #train_data_ind, test_data_ind = model_selection.train_test_split(rated_indexes, test_size=0.20)
+    for el in rated_indexes:
         filtered = movies[movies['ourId'] == el]
         for line2 in filtered.itertuples():
             movie_data = [line2[1], line2[2],line2[3], line2[4], line2[5], line2[6], line2[7], line2[8],
@@ -72,26 +86,26 @@ if __name__=='__main__':
                           line2[25], line2[26], line2[27], line2[28], line2[29], line2[30]]
             rated_movies.append(movie_data)
             ratings_movies.append(str(train_data_matrix[4, el]))
-    """
+
     scaler = StandardScaler()
     scaler.fit(rated_movies)
     X_train = scaler.transform(rated_movies)
 
-    print("poceo")
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(30, 30), random_state=1)
     clf.fit(X_train, ratings_movies)
-    print("zavrsio")
-    """
+
+
     indexes = numpy.argpartition(new_sim, -30)[-30:]
-    print(indexes)
+    test_rated_indexes = test_data_matrix[4].nonzero()[0]
     to_continue = []
     for x in indexes:
-        if x not in rated_indexes:
+        if x in test_rated_indexes:
             to_continue.append(x)
 
+    print(to_continue)
     movies_to_predict = []
     real_ratings = []
-    for el in test_data_ind:
+    for el in test_rated_indexes:
         filtered = movies[movies['ourId'] == el]
         for line2 in filtered.itertuples():
             movie_data = [line2[1], line2[2],line2[3], line2[4], line2[5], line2[6], line2[7], line2[8],
@@ -99,7 +113,7 @@ if __name__=='__main__':
                           line2[17], line2[18], line2[19], line2[20], line2[21], line2[22], line2[23], line2[24],
                           line2[25], line2[26], line2[27], line2[28], line2[29], line2[30]]
             movies_to_predict.append(movie_data)
-            real_ratings.append(str(train_data_matrix[4, el]))
+            real_ratings.append(str(test_data_matrix[4, el]))
     scaler.fit(movies_to_predict)
     X_test = scaler.transform(movies_to_predict)
 
@@ -108,4 +122,4 @@ if __name__=='__main__':
     print(predictions)
     print(confusion_matrix(real_ratings,predictions))
     print(classification_report(real_ratings,predictions))
-    """
+    print 'MLP classifier RMSE: ' + str(eval_mlp(predictions, real_ratings))
